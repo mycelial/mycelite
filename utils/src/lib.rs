cd2541538b8f4ac78af905268ca0af88a25e830d
Alex Wilson <alex@mycelial.com>
2023-02-28 10:27:38 -0500

    cleanup

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,40 +1,41 @@
 // calculates diff between two sqlite pages
 pub fn get_diff<'a>(new_page: &'a [u8], old_page: &'a [u8]) -> Vec<(usize, &'a [u8])> {
-    let mut res: Vec<(usize, &[u8])> = Vec::new();
-
     // TODO?: currently just assuming pages are the same length
-    let mut i: usize = 0;
     let l = old_page.len();
 
     let mut offset = 0;
-    let mut in_change_block = false;
 
-    loop {
-        if i == l && in_change_block {
-            res.push((offset, &new_page[offset..i]));
-            in_change_block = false
-        }
-        if i >= l {
-            break;
-        }
-
-        if new_page[i] != old_page[i] {
-            if !in_change_block {
-                in_change_block = true;
-                offset = i;
+    old_page
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, old_val)| {
+            let new_val = new_page[i];
+            match old_val == &new_val {
+                true => {
+                    // if this value matches and the previous one did not, we know we just passed the end of one or more
+                    // values that didn't match. In that case, we return the offset as well as the values to be changed, which
+                    // are the ones between `offset` and `i`, as results.
+                    if i != 0 && new_page[i - 1] != old_page[i - 1] {
+                        return Some((offset, &new_page[offset..i]));
+                    }
+                    None
+                }
+                false => {
+                    // if this value doesn't match but the previous one did, we know we're at the beginning of one or more
+                    // values that don't match. we note the position by updating `offset` to `i`.
+                    if i == 0 || new_page[i - 1] == old_page[i - 1] {
+                        offset = i;
+                    }
+                    // normally, we add the values that need to be changed as soon as we see a matching value again, but
+                    // when we're on the last value that doesn't match, we need to have special handing to include it.
+                    if i == (l - 1) {
+                        return Some((offset, &new_page[offset..i + 1]));
+                    }
+                    None
+                }
             }
-        }
-        if new_page[i] == old_page[i] {
-            if in_change_block {
-                res.push((offset, &new_page[offset..i]));
-                in_change_block = false
-            }
-        }
-
-        i += 1
-    }
-
-    res
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -58,7 +59,7 @@ mod tests {
     }
 
     #[test]
-    fn test_it_works_with_bytes_at_end_changed() {
+    fn test_it_works_with_values_at_end_changed() {
         let old_page: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         let new_page: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
         let results = get_diff(new_page, old_page);
