3aee889e79bd7634293706c4e4a39cf3806430a3
alexandrinaw <alex@mycelial.com>
2023-03-06 12:39:05 -0500

    Alex/utils bugfix (#40)
    * fix a few bugs related to beginning & end diffs
    * only return diffs against 0s, not entire new page, when old page does not exist
    * make sure we don't return results when there is no difference

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -18,30 +18,29 @@ pub fn get_diff<'a>(
         .zip(new_page)
         .enumerate()
         .filter_map(move |(i, values)| {
-            // special case for when old page is empty
-            if o == 0 && i == (l - 1) {
-                return Some((0, &new_page[0..i + 1]));
-            }
             if values.0 == values.1 {
                 // if this value matches and the previous one did not, we know we just passed the end of one or more
                 // values that didn't match. In that case, we note the position by setting `offset_end` to `i`
-                if i != 0 && i < o && new_page[i - 1] != old_page[i - 1] {
+                if i != 0 && ((i < o && new_page[i - 1] != old_page[i - 1]) || i >= o && new_page[i - 1] != 0) {
                     offset_end = i;
                 }
                 // if we're HEADER_SIZE past the last section that needs changing, or at the end, we need to return the last blob of changes
-                if offset_end + HEADER_SIZE == i || i == (l - 1) {
+                if  offset != offset_end && (offset_end + HEADER_SIZE == i || (i == (l - 1) && offset_end + HEADER_SIZE > i))
+                {
                     return Some((offset, &new_page[offset..offset_end]));
                 }
             } else {
                 // if this value doesn't match but the previous one did, we know we're at the beginning of one or more
                 // values that don't match. we note the position by updating `offset` to `i`.
-                if (i == 0 || (i < o && new_page[i - 1] == old_page[i - 1])) && (offset_end == 0 || offset_end + HEADER_SIZE < i) {
-                    offset = i;
+                if i == 0 || ((i < o && new_page[i - 1] == old_page[i - 1]) || i >= o && new_page[i-1] == 0) {
+                    if offset_end == 0 || offset_end + HEADER_SIZE < i {
+                        offset = i;
+                    }
                 }
                 // normally, we add the values that need to be changed as soon as we see a matching value again, but
                 // when we're on the last value that doesn't match, we need to have special handing to include it.
                 if i == (l - 1) {
-                    return Some((offset, &new_page[offset..i + 1]));
+                    return Some((offset, &new_page[offset..=i]));
                 }
             }
             None
@@ -62,10 +61,32 @@ mod tests {
 
     #[test]
     fn test_it_works_with_actual_data() {
-        let old_page: &[u8] = &[0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3];
-        let new_page: &[u8] = &[0, 1, 2, 3, 1, 1, 1, 1, 2, 3, 1, 3];
+        let old_page: &[u8] = &[
+            83, 81, 76, 105, 116, 101, 32, 102, 111, 114, 109, 97, 116, 32, 51, 0, 16, 0, 1, 1, 0,
+            64, 32, 32, 0, 0, 0, 4, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 46, 99, 1, 13, 0, 0, 0, 1, 15,
+            201, 0, 15, 201, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 53, 1, 6, 23, 21, 21,
+            1, 79, 116, 97, 98, 108, 101, 116, 101, 115, 116, 116, 101, 115, 116, 2, 67, 82, 69,
+            65, 84, 69, 32, 84, 65, 66, 76, 69, 32, 116, 101, 115, 116, 40, 110, 117, 109, 98, 101,
+            114, 32, 105, 110, 116, 101, 103, 101, 114, 41,
+        ];
+        let new_page: &[u8] = &[
+            83, 81, 76, 105, 116, 101, 32, 102, 111, 114, 109, 97, 116, 32, 51, 0, 16, 0, 1, 1, 0,
+            64, 32, 32, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 46, 99, 1, 13, 0, 0, 0, 1, 15,
+            201, 0, 15, 201, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 53, 1, 6, 23, 21, 21,
+            1, 79, 116, 97, 98, 108, 101, 116, 101, 115, 116, 116, 101, 115, 116, 2, 67, 82, 69,
+            65, 84, 69, 32, 84, 65, 66, 76, 69, 32, 116, 101, 115, 116, 40, 110, 117, 109, 98, 101,
+            114, 32, 105, 110, 116, 101, 103, 101, 114, 41,
+        ];
         let results = get_diff(new_page, old_page);
-        let expected: Vec<(usize, &[u8])> = vec![(1, &[1, 2, 3, 1, 1, 1, 1, 2, 3, 1])];
+        let expected: Vec<(usize, &[u8])> = vec![(27, &[5]), (95, &[5])];
         assert_eq!(results.collect::<Vec<(usize, &[u8])>>(), expected);
     }
 
@@ -114,12 +135,23 @@ mod tests {
     }
 
     #[test]
-    fn test_it_works_with_empty_old_page() {
+    fn test_it_works_with_empty_old_page_and_new_page_all_zeros() {
         let old_page: &[u8] = &[];
-        let new_page: &[u8] = &[1, 0];
+        let new_page: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         let results = get_diff(new_page, old_page);
-        let expected: Vec<(usize, &[u8])> = vec![(0, &[1, 0])];
+        let expected: Vec<(usize, &[u8])> = vec![];
+
+        assert_eq!(results.collect::<Vec<(usize, &[u8])>>(), expected);
+    }
+
+    #[test]
+    fn test_it_works_with_empty_old_page() {
+        let old_page: &[u8] = &[];
+        let new_page: &[u8] = &[0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0];
+
+        let results = get_diff(new_page, old_page);
+        let expected: Vec<(usize, &[u8])> = vec![(3, &[1, 1, 1, 0, 1])];
 
         assert_eq!(results.collect::<Vec<(usize, &[u8])>>(), expected);
     }
@@ -131,6 +163,7 @@ mod tests {
             }
             let diff = get_diff(&new, &old);
             let mut brand_new = old.clone();
+
             for (offset, bytes) in diff {
                 for (i, val) in bytes.iter().enumerate() {
                     brand_new[offset + i] = *val;
